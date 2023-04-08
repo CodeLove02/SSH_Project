a8f22a47861ce9cd12cb65567f61ae608716f4d1
CodeLove02 <113576547+CodeLove02@users.noreply.github.com>
2023-04-09 00:31:32 +0900

    로그인 로그아웃 기능 구현

--- src/main/java/com/ssh/example/demo/controller/UsrMemberController.java
+++ src/main/java/com/ssh/example/demo/controller/UsrMemberController.java
@@ -8,15 +8,18 @@ import org.springframework.web.multipart.MultipartRequest;
 
 import com.ssh.example.demo.service.MemberService;
 import com.ssh.example.demo.util.Utility;
+import com.ssh.example.demo.vo.Member;
 import com.ssh.example.demo.vo.ResultData;
+import com.ssh.example.demo.vo.Rq;
 
 @Controller
 public class UsrMemberController {
 	private MemberService memberService;
-	
+	private Rq rq;
 	@Autowired
-	public UsrMemberController(MemberService memberService) {
+	public UsrMemberController(MemberService memberService, Rq rq) {
 		this.memberService = memberService;
+		this.rq= rq;
 	}
 
 	@RequestMapping("/usr/member/join")
@@ -58,6 +61,50 @@ public class UsrMemberController {
 		
 		
 		return Utility.jsReplace(doJoinRd.getMsg(), "/");
+	}
+	
+	@RequestMapping("/usr/member/login")
+	public String showLogin() {
+		return "usr/member/login";
+	}
+
+	@RequestMapping("/usr/member/doLogin")
+	@ResponseBody
+	public String doLogin(String loginId, String loginPw) {
+
+		if (Utility.empty(loginId)) {
+			return Utility.jsHistoryBack("아이디를 입력해주세요");
+		}
+		if (Utility.empty(loginPw)) {
+			return Utility.jsHistoryBack("비밀번호를 입력해주세요");
+		}
+
+		Member member = memberService.getMemberByLoginId(loginId);
+
+		if (member == null) {
+			return Utility.jsHistoryBack("존재하지 않는 아이디입니다");
+		}
+		
+		if (member.getLoginPw().equals(loginPw) == false) {
+			return Utility.jsHistoryBack("비밀번호가 일치하지 않습니다");
+		}
+
+		if (member.isDelStatus() == true) {
+			return Utility.jsHistoryBack("사용할 수 없는 계정입니다");
+		}
+		
+		rq.login(member);
+
+		return Utility.jsReplace(Utility.f("%s님 환영합니다", member.getNickname()), "/");
+	}
+
+	@RequestMapping("/usr/member/doLogout")
+	@ResponseBody
+	public String doLogout() {
+
+		rq.logout();
+
+		return Utility.jsReplace("로그아웃 되었습니다", "/"); 
 	}
 	
 }
---
+++ src/main/java/com/ssh/example/demo/vo/Rq.java
@@ -0,0 +1,64 @@
+package com.ssh.example.demo.vo;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+import com.ssh.example.demo.service.MemberService;
+import com.ssh.example.demo.util.Utility;
+
+import lombok.Getter;
+
+@Component
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class Rq {
+	@Getter
+	private int loginedMemberId;
+	@Getter
+	private Member loginedMember;
+	
+	private HttpServletRequest req;
+	private HttpServletResponse resp;
+	private HttpSession session;
+
+	public Rq(HttpServletRequest req, HttpServletResponse resp, MemberService memberService) {
+		this.req = req;
+		this.resp = resp;
+		this.session = req.getSession();
+		
+		int loginedMemberId = 0;
+		Member loginedMember = null;
+		
+		if(session.getAttribute("loginedMemberId") != null) {
+			loginedMemberId = (int) session.getAttribute("loginedMemberId");
+			loginedMember = memberService.getMemberById(loginedMemberId);
+		}
+		
+		this.loginedMemberId = loginedMemberId;
+		this.loginedMember = loginedMember;
+		
+	}
+
+	
+
+	public void login(Member member) {
+		session.setAttribute("loginedMemberId", member.getId());
+	}
+
+	public void logout() {
+		session.removeAttribute("loginedMemberId");
+	}
+
+	public String jsReturnOnView(String msg, boolean historyBack) {
+		req.setAttribute("msg", msg);
+		req.setAttribute("historyBack", historyBack);
+		return "usr/common/js";
+	}
+	
+}
