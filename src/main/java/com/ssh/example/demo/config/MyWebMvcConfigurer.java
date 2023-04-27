8132d7cdc06d8bf8675d94accc1d91e0443199a3
CodeLove02 <113576547+CodeLove02@users.noreply.github.com>
2023-04-27 21:40:34 +0900

    doAdd 메서드 Write 메서드(jsp로 연결), doWrite 메서드로 변경

--- src/main/java/com/ssh/example/demo/config/MyWebMvcConfigurer.java
+++ src/main/java/com/ssh/example/demo/config/MyWebMvcConfigurer.java
@@ -2,60 +2,33 @@ package com.ssh.example.demo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.ssh.example.demo.interceptor.BeforeActionInterceptor;
 import com.ssh.example.demo.interceptor.NeedLoginInterceptor;
-import com.ssh.example.demo.interceptor.NeedLogoutInterceptor;
 
 @Configuration
 public class MyWebMvcConfigurer implements WebMvcConfigurer {
 
 	private BeforeActionInterceptor beforeActionInterceptor;
 	private NeedLoginInterceptor needLoginInterceptor;
-	private NeedLogoutInterceptor needLogoutInterceptor;
-	
-	
+
 	@Autowired
 	public MyWebMvcConfigurer(BeforeActionInterceptor beforeActionInterceptor,
-			NeedLoginInterceptor needLoginInterceptor, NeedLogoutInterceptor needLogoutInterceptor) {
+			NeedLoginInterceptor needLoginInterceptor) {
 		this.beforeActionInterceptor = beforeActionInterceptor;
 		this.needLoginInterceptor = needLoginInterceptor;
-		this.needLogoutInterceptor = needLogoutInterceptor;
 	}
-	
-	
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		
-		InterceptorRegistration ir;
-		
-		ir = registry.addInterceptor(beforeActionInterceptor);
-		ir.addPathPatterns("/**");
-		ir.excludePathPatterns("/resource/**");
-		ir.excludePathPatterns("/error");
+		registry.addInterceptor(beforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("/resource/**")
+				.excludePathPatterns("/error");
 
-		ir = registry.addInterceptor(needLoginInterceptor);
-		ir.addPathPatterns("/usr/article/write");
-		ir.addPathPatterns("/usr/article/doWrite");
-		ir.addPathPatterns("/usr/article/doDelete");
-		ir.addPathPatterns("/usr/article/modify");
-		ir.addPathPatterns("/usr/article/doModify");
-		ir.addPathPatterns("/usr/member/doLogout");
-		ir.addPathPatterns("/usr/member/myPage");
-		ir.addPathPatterns("/usr/member/modify");
-		ir.addPathPatterns("/usr/member/doModify");
-		
-		
-		
-		ir = registry.addInterceptor(needLogoutInterceptor);
-		ir.addPathPatterns("/usr/member/login");
-		ir.addPathPatterns("/usr/member/doLogin");
-		ir.addPathPatterns("/usr/member/join");
-		ir.addPathPatterns("/usr/member/doJoin");
+		registry.addInterceptor(needLoginInterceptor).addPathPatterns("/usr/article/write")
+				.addPathPatterns("/usr/article/doWrite").addPathPatterns("/usr/article/doDelete")
+				.addPathPatterns("/usr/article/modify").addPathPatterns("/usr/article/doModify");
 	}
 
 }
--- src/main/webapp/WEB-INF/jsp/usr/article/write.jsp
+++ src/main/webapp/WEB-INF/jsp/usr/article/write.jsp
@@ -2,12 +2,10 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <c:set var="pageTitle" value="ARTICLE WRITE" />
 <%@ include file="../common/head.jsp"%>
-<%@ include file="../common/toastUiEditorLib.jsp"%>
 
 <section class="mt-8 text-xl">
 	<div class="container mx-auto px-3">
-		<form action="doWrite" method="POST" onsubmit="submitForm(this); return false;">
-			<input type="hidden" name="body"/>
+		<form action="doWrite" method="POST">
 			<div class="table-box-type-1">
 				<table class="table table-zebra w-full">
 					<colgroup>
@@ -16,33 +14,12 @@
 
 					<tbody>
 						<tr>
-							<th>게시판</th>
-							<td>
-<!-- 								<select name="boardId"> -->
-<!-- 									<option>게시판을 선택해주세요</option> -->
-<!-- 									<option value="1">공지사항</option> -->
-<!-- 									<option value="2">자유</option> -->
-<!-- 								</select> -->
-								<label>
-									<input type="radio" name="boardId" value="1" checked/>&nbsp;공지사항
-								</label>
-								&nbsp;&nbsp;&nbsp;
-								<label>
-									<input type="radio" name="boardId" value="2"/>&nbsp;자유
-								</label>
-							</td>
-						</tr>
-						<tr>
 							<th>제목</th>
 							<td><input class="input input-bordered w-full max-w-xs" type="text" name="title" placeholder="제목을 입력해주세요" /></td>
 						</tr>
 						<tr>
 							<th>내용</th>
-							<td>
-								<div class="toast-ui-editor">
-      								<script type="text/x-template"></script>
-    							</div>
-							</td>
+							<td><textarea class="textarea textarea-bordered w-full" name="body" placeholder="내용을 입력해주세요"></textarea></td>
 						</tr>
 						<tr>
 							<td colspan="2"><button class="btn btn-active btn-ghost">작성</button></td>
