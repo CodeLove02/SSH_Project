7129fe95c01029ba4ba16486977cbff4ad50c626
CodeLove02 <113576547+CodeLove02@users.noreply.github.com>
2023-05-04 03:10:29 +0900

    arrt테이블 추가. attr테이블을 활용하기위한 코드 추가

--- src/main/java/com/ssh/example/demo/util/Utility.java
+++ src/main/java/com/ssh/example/demo/util/Utility.java
@@ -1,10 +1,9 @@
 package com.ssh.example.demo.util;
 
-import com.ssh.example.demo.util.Utility;
+import java.text.SimpleDateFormat;
 
 public class Utility {
 
-	
 	public static boolean empty(Object obj) {
 		
 		if(obj == null) {
@@ -19,12 +18,12 @@ public class Utility {
 			
 		return str.trim().length() == 0;
 	}
-	
+
 	public static String f(String format, Object... args) {
 		return String.format(format, args);
 	}
-	
-public static String jsHistoryBack(String msg) {
+
+	public static String jsHistoryBack(String msg) {
 		
 		if(msg == null) {
 			msg = "";
@@ -41,27 +40,47 @@ public static String jsHistoryBack(String msg) {
 						""", msg);
 	}
 
-public static String jsReplace(String msg, String uri) {
-	if(msg == null) {
-		msg = "";
+	public static String jsReplace(String msg, String uri) {
+		if(msg == null) {
+			msg = "";
+		}
+		
+		if(uri == null) {
+			uri = "";
+		}
+		
+		return Utility.f("""
+						<script>
+							const msg = '%s'.trim();
+							if(msg.length > 0){
+								alert(msg);
+							}
+							location.replace('%s');
+						</script>
+						""", msg, uri);
 	}
 	
-	if(uri == null) {
-		uri = "";
+	public static String getDateStrLater(long seconds) {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+		String dateStr = format.format(System.currentTimeMillis() + seconds * 1000);
+
+		return dateStr;
 	}
-	
-	return Utility.f("""
-					<script>
-						const msg = '%s'.trim();
-						if(msg.length > 0){
-							alert(msg);
-						}
-						location.replace('%s');
-					</script>
-					""", msg, uri);
-}
 
+	public static String getTempPassword(int length) {
+		int index = 0;
+		char[] charArr = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
+				'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
-	
-	
+		StringBuffer sb = new StringBuffer();
+
+		for (int i = 0; i < length; i++) {
+			index = (int) (charArr.length * Math.random());
+			sb.append(charArr[index]);
+		}
+
+		return sb.toString();
+	}
+
 }
